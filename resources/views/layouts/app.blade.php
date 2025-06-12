2d9481bf78c33d8cf35d3fa1d4e3ae85d3bfd6cd
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-06-12 22:29:27 +0700

    Add main layout template for the application

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+{{-- filepath: c:\Users\ryans\Documents\PROJECT DEVELOP\A Github\car-rental-system\resources\views\layouts\app.blade.php --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>{{ config('app.name', 'Car Rental System') }}</title>
+    
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    @yield('styles')
+</head>
+<body>
+    <div id="app">
+        @include('components.navbar')
+        
+        <main>
+            @yield('content')
+        </main>
+        
+        @include('components.footer')
+    </div>
+    
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    
+    @yield('scripts')
+</body>
+</html>
